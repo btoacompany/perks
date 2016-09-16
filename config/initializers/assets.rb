651f88fe0a3a99ccd8ba66380d9e47a77fdd4f4c
sayorik <s.karakama@btoa-company.com>
2016-09-17 06:16:45 +0900

    fixed jquery assets error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( default2.css jquery.easytabs.js )
+Rails.application.config.assets.precompile += %w( default2.css jquery.easytabs.js introjs.css
+intro.js canvasjs.min.js jquery.canvas.min.js )
