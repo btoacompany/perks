d41d2d6b70addb59763ac6f2f35445fc97f0a00d
sayorik <s.karakama@btoa-company.com>
2016-10-10 18:47:05 +0900

    deploy.rb cap version edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
 set :application, 'perks'
 set :repo_url, 'git@github.com:btoacompany/perks.git' # Edit this to match your repository
 set :branch, :master
