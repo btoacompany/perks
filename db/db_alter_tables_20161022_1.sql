60b2edf9d3dfc973aa467ecd3c283289d503a8b9
sayorik <s.karakama@btoa-company.com>
2016-10-25 22:16:40 +0900

    added db alter

--- db/db_alter_tables_20161022_1.sql
+++ db/db_alter_tables_20161022_1.sql
@@ -1,3 +1,5 @@
 ALTER TABLE request_rewards ADD COLUMN reward_prizy_id INT DEFAULT 0 AFTER reward_id;
 ALTER TABLE request_rewards CHANGE reward_id reward_id INT DEFAULT 0 AFTER user_id;
 ALTER TABLE company ADD COLUMN plan_start_date datetime AFTER plan_approved;
+ALTER TABLE company ADD COLUMN bonus_default INT DEFAULT 0 AFTER bonus_points;
+ALTER TABLE company ADD COLUMN points_default INT DEFAULT 0 AFTER hashtags;
