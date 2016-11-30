2c023120ed252a1da67a757080ff42afb8170d64
sayorik <s.karakama@btoa-company.com>
2016-12-01 00:04:21 +0900

    reset points

--- batch/reset_giveaway_points.rb
+++ batch/reset_giveaway_points.rb
@@ -2,6 +2,10 @@ users = User.where(delete_flag: 0)
 
 users.each do | user |
   user[:out_points] = 150
+  if user[:company_id] == 26
+    user[:out_points] = 3000 
+    
+  end
   user.save
 
   sleep(1)
