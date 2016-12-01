1616589685d663f3701930389a7138779a0a9eb8
sayorik <s.karakama@btoa-company.com>
2016-12-01 22:13:22 +0900

    fixed rewards bugs

--- batch/reset_giveaway_points.rb
+++ batch/reset_giveaway_points.rb
@@ -1,12 +1,20 @@
 users = User.where(delete_flag: 0)
 
 users.each do | user |
-  user[:out_points] = 150
-  if user[:company_id] == 26
-    user[:out_points] = 3000 
-    
-  end
+  company = Company.find(user[:company_id])
+  user[:out_points] = company.points_default 
+
+  puts "User: #{user[:company_id]} - #{user[:out_points]}"
   user.save
 
-  sleep(1)
+  sleep(0.1)
+end
+
+companies = Company.where(delete_flag: 0)
+
+companies.each do | c |
+  c.bonus_points = c.bonus_default
+  puts "Company: #{c[:id]} - #{c[:bonus_points]}"
+  c.save
+  sleep(0.1)
 end
