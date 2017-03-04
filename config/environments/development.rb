e52e429d5ea643d81eab38ff061920216ff8be6c
naoto udagawa <n.udagawa@btoa-company.com>
2017-03-04 18:48:02 +0900

    two_step_authentication_to_login_gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,10 +20,10 @@ Rails.application.configure do
   #config.action_mailer.delivery_method = :sendmail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address	  => "smtp.gmail.com",
-    :port	  => 587,
-    :user_name	  => ENV['EMAIL_USERNAME'],
-    :password	  => ENV['EMAIL_PASSWORD'],
+    :address    => "smtp.gmail.com",
+    :port    => 587,
+    :user_name    => ENV['EMAIL_USERNAME'],
+    :password    => ENV['EMAIL_PASSWORD'],
     :authentication => "plain",
     :enable_starttls_auto => true
   }
