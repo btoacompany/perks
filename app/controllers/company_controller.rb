e3cdb9658ca9d5309710c697964fbd938d77ecf0
naoto udagawa <n.udagawa@btoa-company.com>
2017-04-21 11:51:10 +0900

    update_csv_error

--- app/controllers/company_controller.rb
+++ app/controllers/company_controller.rb
@@ -302,7 +302,7 @@ class CompanyController < ApplicationController
         redirect_to '/company/employees/register'
       else
         flash[:notice_about_create_user] = "社員#{count_created_user_by_csv}名を追加しました"
-        session[:current_invite_email_setting] = company.invite_email_flag
+        # session[:current_invite_email_setting] = company.invite_email_flag
         redirect_to '/company/employees/register'
       end
     else
