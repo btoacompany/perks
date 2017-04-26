fe85c6559e043c36d06ebf9e052b79abf4d2e24d
udagawa-naoto <n.udagawa@btoa-company.com>
2017-04-26 16:26:50 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session,
     if: Proc.new { |c| c.request.format =~ %r{application/json} }
-  helper_method :current_userz
+  helper_method :current_user
 
   before_filter :init_url
 
