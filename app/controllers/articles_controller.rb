dcb5bd081136b13107a7d2a88d5180ccd6ec5f2f
naoto udagawa <n.udagawa@btoa-company.com>
2017-10-31 02:21:25 +0900

    update_afew_bugs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
-  before_filter :init_url
   impressionist actions: [:index, :show]
   before_action :init
+  # before_filter :init_url
 
   def init
     if session[:email].present? || cookies[:email].present?
@@ -18,6 +18,13 @@ class ArticlesController < ApplicationController
       @company = Company.find(@id)
     else
       logout
+    end
+    if Rails.env.production?
+      @s3_url = "https://s3-ap-northeast-1.amazonaws.com/prizy"
+      @s3_bucket = "prizy"
+    elsif Rails.env.development?
+      @s3_url = "https://s3-ap-northeast-1.amazonaws.com/btoa-img"
+      @s3_bucket = "btoa-img"
     end
   end
 
