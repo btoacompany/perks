34495ebde55f308173355b00df421ccec4b50a36
naoto udagawa <n.udagawa@btoa-company.com>
2017-12-11 17:49:36 +0900

    update

--- app/jobs/release_article_job.rb
+++ app/jobs/release_article_job.rb
@@ -5,5 +5,10 @@ class ReleaseArticleJob < ActiveJob::Base
   def perform(*args)
     # Do something later
     CompanyMailer.release_article(args[0][:user_emails], args[0][:subject], args[0][:description]).deliver_now
+    rescue => e
+    if e
+      flash[:notice] = "#{e}"
+      redirect_to company_articles_path
+    end
   end
 end
