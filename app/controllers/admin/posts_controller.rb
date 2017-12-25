3998c2ba59e77965db776a5c66080fcd0f1967c3
naoto udagawa <n.udagawa@btoa-company.com>
2017-12-25 16:53:50 +0900

    export_messages

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,28 +1,31 @@
 class Admin::PostsController < Admin::Base
   require 'csv'
+  require 'nkf'
 
   def index
     @posts = Post.where(company_id: @company.id, delete_flag: 0).order(create_time: :desc).paginate(page: params[:page], per_page: 40)
     @teams = Team.where(company_id: @company.id)
-    # @teams = Team.teams(@company.id)
   end
 
   def export_all_posts
-    @posts = Post.where(company_id: @company.id, delete_flag: 0)
-    respond_to do |format|
-      format.html
-      format.csv { send_data @posts.to_csv }
+    if params[:period] == "three_month"
+      period = Date.today - 3.months
+    elsif params[:period] == "six_month"
+      period = Date.today - 6.months
+    elsif params[:period] == "twelve_month"
+      period = Date.today - 12.months
     end
+    @posts = Post.where(company_id: @company.id, delete_flag: 0).where('create_time >= ?', period)
 
-    # headers = %w(日時 送信者 受信者 内容)
-    # data = CSV.generate(encoding: Encoding::SJIS, headers: headers ) do |csv|
-    #   csv << headers
-    #   @posts.each do |post|
-    #     csv << [post.create_time.strftime("%Y/%m/%d %H:%M:%S"), post.user.fullname, post.receiver.fullname, post.description]
-    #   end
-    # end
-    # datetime = Time.now
-    # format_datetime = datetime.strftime('%Y_%m_%d_%H%M')
-    # send_data(data , filename:  "#{format_datetime}" + "社員登録フォーマット" + '.csv',type: 'csv')
+    headers = %w(日時 送信者 受信者 内容)
+    csv_str = CSV.generate do |csv|
+      csv << headers
+        @posts.all.each do |post|
+        csv << [post.create_time.strftime("%Y/%m/%d %H:%M:%S"), post.user.fullname, post.receiver.fullname, post.description]
+      end
+    end
+    datetime = Time.now
+    format_datetime = datetime.strftime('%Y_%m_%d_%H%M')
+    send_data csv_str, filename: "#{format_datetime}" + "メッセージ一覧" , type: 'text/csv;'
   end
 end
