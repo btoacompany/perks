ea45c8cfaa20d528d321729ad10be5265cbce166
naoto udagawa <n.udagawa@btoa-company.com>
2017-12-26 16:47:43 +0900

    feature deliver contact mails

---
+++ app/controllers/admin/contacts_controller.rb
@@ -0,0 +1,22 @@
+class Admin::ContactsController < Admin::Base
+  def index
+
+  end
+
+  def new
+  end
+
+  def create
+    user_emails = User.where(company_id: @company.id, delete_flag: 0).pluck(:email)
+    logger.debug(user_emails.count)
+    data = {
+      user_emails: user_emails,
+      subject: params[:subject],
+      description: params[:description]
+    }
+    DeliverContactMailJob.new.async.perform(data)
+
+    flash[:notice] = "メール配信が完了しました"
+    redirect_to new_admin_contact_path
+  end
+end
---
+++ app/jobs/deliver_contact_mail_job.rb
@@ -0,0 +1,12 @@
+class DeliverContactMailJob < ActiveJob::Base
+  queue_as :default
+  include SuckerPunch::Job
+
+  def perform(*args)
+    # Do something later
+    args[0][:user_emails].each do |email|
+      CompanyMailer.deliver_contact_mail(email, args[0][:subject], args[0][:description]).deliver_now
+      sleep(0.8)
+    end
+  end
+end
