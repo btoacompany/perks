6bda4d2f9cbc28acef13afaeb0f27b16cbb8e869
naoto udagawa <n.udagawa@btoa-company.com>
2018-01-14 13:53:18 +0900

    update_export_csv

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -16,7 +16,7 @@ class Admin::PostsController < Admin::Base
       period = Date.today - 12.months
     end
     @posts = Post.where(company_id: @company.id, delete_flag: 0).where('create_time >= ?', period)
-    @teams = Team.where(company_id: @company.id)
+    teams = Team.where(company_id: @company.id)
 
     headers = %w(日時 所属 送信者 所属 受信者 内容)
     csv_str = CSV.generate do |csv|
@@ -27,14 +27,16 @@ class Admin::PostsController < Admin::Base
             user_assigned_team = ""
             receiver_assigned_team = ""
             receiver_name = ""
-            @teams.each do |team|
-              if team.member_ids.present? && team.member_ids.include?(post.user_id.to_s)
-                department = Department.find(team.department_id)
-                user_assigned_team = department.try(:dep_name) + "/" + team.team_name
+
+
+            teams.each do |t|
+              if t.member_ids.present? && t.member_ids.include?(post.user.id.to_s)
+                user_assigned_team = "#{t.department.try(:dep_name)} #{t.try(:team_name)}"
+                break
               end
-              if team.member_ids.present? && team.member_ids.include?(receiver_id)
-                department = Department.find_by(id: team.department_id)
-                receiver_assigned_team = "#{department.try(:dep_name)} / #{team.try(:team_name)}"
+              user_assigned_team = "所属がありません" unless user_assigned_team.present?
+              if t.member_ids.present? && t.member_ids.include?(post.receiver_id)
+                receiver_assigned_team = "#{t.department.try(:dep_name)} #{t.try(:team_name)}"
                 user = User.find_by(id: receiver_id.to_i)
                 receiver_name = "#{user.try(:lastname)}" "#{user.try(:lastname)}"
               end
@@ -44,15 +46,17 @@ class Admin::PostsController < Admin::Base
         else
           user_assigned_team = ""
           receiver_assigned_team = ""
-          @teams.each do |team|
-            if team.member_ids.present? && team.member_ids.include?(post.user_id.to_s)
-              department = Department.find_by(id: team.department_id)
-              user_assigned_team = department.try(:dep_name) + "/" +team.team_name
+          teams.each do |t|
+            if t.member_ids.present? && t.member_ids.include?(post.user.id.to_s)
+              user_assigned_team = "#{t.department.try(:dep_name)} #{t.try(:team_name)}"
+              break
             end
-            if team.member_ids.present? && team.member_ids.include?(post.receiver_id.to_s)
-              department = Department.find_by(id: team.department_id)
-              receiver_assigned_team = "#{department.try(:dep_name)} / #{team.try(:team_name)}"
+            user_assigned_team = "所属がありません" unless user_assigned_team.present?
+            if t.member_ids.present? && t.member_ids.include?(post.receiver_id.to_s)
+              receiver_assigned_team = "#{t.department.try(:dep_name)} #{t.try(:team_name)}"
+              break
             end
+            receiver_assigned_team = "所属がありません" unless receiver_assigned_team.present?
           end
           csv << [post.create_time.strftime("%Y/%m/%d %H:%M:%S"), user_assigned_team, post.user.try(:lastname) + post.user.try(:lastname), receiver_assigned_team, post.receiver.try(:lastname) + post.receiver.try(:lastname), post.description]
         end
