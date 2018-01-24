1f9a3c12542228be077eb71d5b933356b72d2710
naoto udagawa <n.udagawa@btoa-company.com>
2018-01-24 15:34:42 +0900

    partial sidebar

--- app/controllers/admin/teams_controller.rb
+++ app/controllers/admin/teams_controller.rb
@@ -2,4 +2,22 @@ class Admin::TeamsController < Admin::Base
   def index
     @deps = Department.of_company(@company.id).available
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
