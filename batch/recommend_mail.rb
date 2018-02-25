fdc2f001d29d446e4b5ff0bebc3806ca32b45303
naoto udagawa <n.udagawa@btoa-company.com>
2018-02-25 20:59:26 +0900

    update recommend mail

--- batch/recommend_mail.rb
+++ batch/recommend_mail.rb
@@ -1,21 +1,26 @@
-# users = User.of_company(@company.id).available
-# teams = Team.of_company(@company.id).available
+company_ids = [1]
+company_ids.each do |company_id|
+  users = User.of_company(company_id).available
+  teams = Team.of_company(company_id).available
+
+  users.each 
+end
 
 
-# users.each do |user|
-#   # targets = [{user1: nil, team: nil}, {user2: nil, team: nil}]
-#   targets = Array.new
-#   selected_user_ids = Array.new
-#   # 
-#   tags = Article.joins(:tag).pluck(:user_id)
-#   target_users = users.where.not(id: user.id).sample(2)
-#   teams.map { |team| targets.push({user: user, team: team}) if team.member_ids.include?(user.id) }
+users.each do |user|
+  # targets = [{user1: nil, team: nil}, {user2: nil, team: nil}]
+  targets = Array.new
+  selected_user_ids = Array.new
+  # 
+  tags = Article.joins(:tag).pluck(:user_id)
+  target_users = users.where.not(id: user.id).sample(2)
+  teams.map { |team| targets.push({user: user, team: team}) if team.member_ids.include?(user.id) }
   
 
-#   data = {
-#     email: user.email,
-#     target: targets
-#   }
-#   CompanyMailer.recommend_mail(data).deliver_now
-#   sleep(0.5)
-# end
+  data = {
+    email: user.email,
+    target: targets
+  }
+  CompanyMailer.recommend_mail(data).deliver_now
+  sleep(0.5)
+end
