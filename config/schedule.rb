4c526a249e6083bb67f89d24426c0c71191e089f
naoto udagawa <n.udagawa@btoa-company.com>
2018-03-05 11:54:14 +0900

    check_recommend_batch

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,6 +24,6 @@ end
 
 # Learn more: http://github.com/javan/whenever
 
-# every 1.minutes do
-#   runner "ArticlesController.batch"
-# end
+every :monday, :at => '00:00 pm' do
+  runner "ArticlesController.batch"
+end
