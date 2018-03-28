17fa0900c91b48ebb5f4100a99aa2f367056ea10
naoto udagawa <n.udagawa@btoa-company.com>
2018-03-28 19:54:12 +0900

    UpdateMessageMaxLength

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,7 +14,7 @@ class Post < ActiveRecord::Base
   before_create :set_create_time
   before_update :set_update_time
 
-  validates :description , length: {maximum: 250}
+  validates :description , length: {maximum: 1024}
 
   scope :available, -> { where(delete_flag: 0) }
   scope :of_company, ->(company_id) { where(company_id: company_id) }
