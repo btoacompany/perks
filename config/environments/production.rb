de2909cc9d9df9f220fcdd9383c516a20d9e3187
naoto udagawa <n.udagawa@btoa-company.com>
2018-05-18 18:29:36 +0900

    SetUpSendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,12 +71,13 @@ Rails.application.configure do
   #config.action_mailer.delivery_method = :sendmail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address	  => "smtp.gmail.com",
-    :port	  => 587,
-    :user_name	  => ENV['EMAIL_USERNAME'],
-    :password	  => ENV['EMAIL_PASSWORD'],
-    :authentication => "plain",
-    :enable_starttls_auto => true
+    port: 587,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "prizy.me",
+    address: "smtp.sendgrid.net",
+    authentication: "plain",
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
