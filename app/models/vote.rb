3a734312c951420cf45c7541edb2c209b8812d37
naoto udagawa <n.udagawa@btoa-company.com>
2018-07-25 13:08:51 +0900

    Add validation  not_before_today to the model vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,9 @@ class Vote < ActiveRecord::Base
   validates :date, presence: true
   validates :title, presence: true, length: { maximum: 40 }
   validates :question, presence: true, length: { maximum: 50 }
+  validate :not_before_today
+
+  def not_before_today
+    errors.add(:date, '明日以降の日付を選択してください') if date.nil? || date <= Date.today
+  end
 end
