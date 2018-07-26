b9179ba469b11d0c10a3f18ecdc00e2505dcd6a9
naoto udagawa <n.udagawa@btoa-company.com>
2018-07-26 13:47:30 +0900

    Add scope to vote models

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -7,6 +7,9 @@ class Vote < ActiveRecord::Base
   validate :not_before_today
 
   scope :of_company, ->(company_id) { where(company_id: company_id) }
+  scope :finished, -> { where("date <= ?", Date.today) }
+  scope :scheduled, -> { where("date > ?", Date.today) }
+  scope :date_order, ->(order = :desc) { order(date: order) }
 
   def not_before_today
     errors.add(:date, 'を明日以降で選択してください') if date.nil? || date <= Date.today
--- app/models/vote_result.rb
+++ app/models/vote_result.rb
@@ -2,4 +2,6 @@ class VoteResult < ActiveRecord::Base
   belongs_to :user
   belongs_to :company
   belongs_to :vote
+
+  scope :of_company, ->(company_id) { where(company_id: company_id) }
 end
