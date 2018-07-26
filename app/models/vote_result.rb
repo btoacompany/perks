faae2fb7caed8348c46f67fdf6ffde15b598e860
naoto udagawa <n.udagawa@btoa-company.com>
2018-07-26 20:54:52 +0900

    Add validation to uniqueness vote_id and user_id

--- app/models/vote_result.rb
+++ app/models/vote_result.rb
@@ -8,4 +8,6 @@ class VoteResult < ActiveRecord::Base
   scope :of_company, ->(company_id) { where(company_id: company_id) }
   scope :of_department, ->(department_id) { where(department_id: department_id) }
   scope :of_vote, ->(vote_id) { where(vote_id: vote_id) }
+
+  validates :vote_id, uniqueness: { scope: :user_id }
 end
