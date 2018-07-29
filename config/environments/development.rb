456a0e6b61bb579bbcf4000ddd8c231ad76c1804
naoto udagawa <n.udagawa@btoa-company.com>
2018-07-29 17:00:20 +0900

    Add gem letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,16 +17,19 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener
+
   #config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address    => "smtp.gmail.com",
-    :port    => 587,
-    :user_name    => ENV['EMAIL_USERNAME'],
-    :password    => ENV['EMAIL_PASSWORD'],
-    :authentication => "plain",
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address    => "smtp.gmail.com",
+  #   :port    => 587,
+  #   :user_name    => ENV['EMAIL_USERNAME'],
+  #   :password    => ENV['EMAIL_PASSWORD'],
+  #   :authentication => "plain",
+  #   :enable_starttls_auto => true
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
